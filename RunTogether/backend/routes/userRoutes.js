72d27fc7e073fdff0c78e9ac9db5a1e3848182f7
yuvals <yuvalsaybak@cs.colman.ac.il>
2024-04-28 11:05:57 +0300

    fixing the navigation bar in the profile page

--- RunTogether/backend/routes/userRoutes.js
+++ RunTogether/backend/routes/userRoutes.js
@@ -1,26 +1,25 @@
-const express = require('express');
+const express = require("express");
 const {
-    createUser,
-    login,
-    getUserByEmail,
-    updateUserProfile,
-    deleteUser
-} = require('../controllers/userController.js');
+  createUser,
+  login,
+  getUserByEmail,
+  updateUserProfile,
+  deleteUser,
+} = require("../controllers/userController.js");
 
 const router = express.Router();
 
-
 // Create a new user
-router.post('/', createUser);
+router.post("/", createUser);
 
 // Get user by ID
-router.post('/login', login);
+router.post("/login", login);
 
 // Update user profile
-router.put('/:email', updateUserProfile);
+router.put("/:email", updateUserProfile);
 
 // Delete user
-router.delete('/:email', deleteUser);
+router.delete("/:email", deleteUser);
 
 //export apiClient aswell
 module.exports = router;
--- RunTogether/screens/BottomNavigation.jsx
+++ RunTogether/screens/BottomNavigation.jsx
@@ -1,60 +1,220 @@
-// import React from 'react';
-// import { View, TouchableOpacity, StyleSheet } from 'react-native';
-// import { Feather } from "@expo/vector-icons";
+import React, { useState, useRef, useEffect } from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Animated,
+  PanResponder,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-// const [showMenu, setShowMenu] = useState(false);
+const BottomNavigation = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [menuPosition, setMenuPosition] = useState(new Animated.Value(1));
+  const navigation = useNavigation();
 
-// const BottomNavigation = ({ activeScreen, navigation }) => {
-//   const goToHomeScreen = () => {
-//     navigation.navigate('Home');
-//   }
+  const handleLogout = async () => {
+    try {
+      console.log("Logout successful");
+      navigation.navigate("Login");
+    } catch (error) {
+      console.error("Error logging out:", error.message);
+    }
+  };
 
-//   const goToPostScreen = () => {
-//     navigation.navigate('Create Post');
-//   }
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
-//   const goToProfileScreen = () => {
-//     navigation.navigate('Profile Details');
-//   }
+  const handlePressOutsideMenu = () => {
+    if (showMenu) {
+      setShowMenu(false);
+    }
+  };
 
-//   const toggleMenu = () => {
-//     setShowMenu(!showMenu);
-// };
+  const handlePanResponderGrant = (e, gestureState) => {
+    e.stopPropagation();
+  };
 
-//   return (
-//     <View style={styles.bottomNavigation}>
-//       <TouchableOpacity onPress={goToHomeScreen}>
-//         <View style={[styles.icon, activeScreen === 'Home' && styles.activeIcon]}>
-//           <Feather name="home" size={22} color="#F7706EFF" />
-//         </View>
-//       </TouchableOpacity>
-//       <TouchableOpacity onPress={goToPostScreen}>
-//         <View style={[styles.icon, activeScreen === 'Create Post' && styles.activeIcon]}>
-//           <Feather name="plus-circle" size={32} color="#F7706EFF" />
-//         </View>
-//       </TouchableOpacity>
-//       <TouchableOpacity onPress={toggleMenu}>
-//         <View style={[styles.icon, activeScreen === 'Menu' && styles.activeIcon]}>
-//           <Feather name="menu" size={22} color="#F7706EFF" />
-//         </View>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// };
+  const panResponder = useRef(
+    PanResponder.create({
+      onPanResponderGrant: handlePanResponderGrant,
+    })
+  ).current;
 
-// const styles = StyleSheet.create({
-//   bottomNavigation: {
-//     flexDirection: 'row',
-//     justifyContent: 'space-around',
-//     paddingVertical: 16,
-//     paddingHorizontal: 16,
-//   },
-//   icon: {
-//     opacity: 0.5,
-//   },
-//   activeIcon: {
-//     opacity: 1,
-//   },
-// });
+  const goToPostScreen = () => {
+    navigation.navigate("Create Post");
+  };
 
-// export default BottomNavigation;
+  const goToProfileScreen = () => {
+    navigation.navigate("Profile Details");
+  };
+
+  useEffect(() => {
+    Animated.timing(menuPosition, {
+      toValue: showMenu ? 0 : 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, [showMenu]);
+
+  return (
+    <TouchableWithoutFeedback onPress={handlePressOutsideMenu}>
+      <View style={styles.container}>
+        <View style={styles.bottomNavigation}>
+          <TouchableOpacity>
+            <View style={styles.icon}>
+              <Feather name="home" size={22} color="#F7706EFF" />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={goToPostScreen}>
+            <View style={styles.icon}>
+              <Feather name="plus-circle" size={32} color="#F7706EFF" />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={toggleMenu}>
+            <View style={styles.icon}>
+              <Feather name="menu" size={22} color="#F7706EFF" />
+            </View>
+          </TouchableOpacity>
+        </View>
+        {showMenu && (
+          <Animated.View
+            style={[
+              styles.menuContainer,
+              { transform: [{ translateY: menuPosition }] },
+            ]}
+            {...panResponder.panHandlers}
+          >
+            <View style={styles.menuHeader}>
+              <Text style={styles.menuHeaderText}>Menu Actions</Text>
+              <View style={styles.menuHeaderLine} />
+            </View>
+            <TouchableOpacity onPress={handleLogout} style={styles.menuAction}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Feather
+                  name="log-out"
+                  size={18}
+                  color="#F7706EFF"
+                  style={{ marginRight: 8 }}
+                />
+                <Text style={styles.menuActionText}>Log Out</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuAction}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Feather
+                  name="file-text"
+                  size={18}
+                  color="#F7706EFF"
+                  style={{ marginRight: 8 }}
+                />
+                <Text style={styles.menuActionText}>My Posts</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={goToProfileScreen}
+              style={styles.menuAction}
+            >
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Feather
+                  name="user"
+                  size={18}
+                  color="#F7706EFF"
+                  style={{ marginRight: 8 }}
+                />
+                <Text style={styles.menuActionText}>My Profile</Text>
+              </View>
+            </TouchableOpacity>
+          </Animated.View>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  bottomNavigation: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: -4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  icon: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 12,
+    borderRadius: 10,
+  },
+  menuContainer: {
+    position: "absolute",
+    bottom: 86,
+    left: 0,
+    right: 0,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: -4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  menuAction: {
+    padding: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  menuActionText: {
+    fontSize: 19,
+    flex: 1,
+    marginLeft: 10,
+  },
+  menuHeader: {
+    backgroundColor: "#F7706EFF",
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  menuHeaderText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+    backgroundColor: "#F7706EFF",
+    paddingVertical: 7,
+    paddingHorizontal: 13,
+  },
+});
+
+export default BottomNavigation;
