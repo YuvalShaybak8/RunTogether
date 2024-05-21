74f369d8f5d4002bcbbf3d26f75626aa0456339c
yuvals <yuvalsaybak@cs.colman.ac.il>
2024-05-21 15:10:39 +0300

    test

--- RunTogether/backend/api/client.js
+++ RunTogether/backend/api/client.js
@@ -5,10 +5,10 @@ import Constants from "expo-constants";
 // const IP = expoConfig.extra.IP;
 // const IP2 = process.env.IP;
 
-// export default axios.create({ baseURL: `http://192.168.3.5:5000` });
+export default axios.create({ baseURL: `http://192.168.3.5:5000` }); // yuval pc
 
-// export default axios.create({ baseURL: `http://172.20.10.2:5000` });
+// export default axios.create({ baseURL: `http://172.20.10.2:5000` }); // yuval iphone up
 
-export default axios.create({ baseURL: `http://10.100.102.5:5000` });
+// export default axios.create({ baseURL: `http://10.100.102.5:5000` }); // tal pc
 
 // export default axios.create({ baseURL: "http://172.20.10.4:5000" }); // tal's phone ip
